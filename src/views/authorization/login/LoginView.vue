3362ce3c12e5c882d10838552773891ea237a79e
Denis Gukov <denis.guk@konomic.com>
2024-09-30 21:14:43 +0300

    auth: fixed styles, added script to update ui-kit

--- src/views/authorization/login/LoginView.vue
+++ src/views/authorization/login/LoginView.vue
@@ -1,24 +1,22 @@
 <template>
 	<section :class="$style[className]">
-    <h1>Login</h1>
-    <div>
-      <OLabel caption="E-mail">
-        <OInput />
-      </OLabel>
-      <OLabel caption="Password">
-        <OInput />
-      </OLabel>
-      <OButton text="Войти"/>
-      <span>
-        Нет аккаунта? <span class="r-pointer" @click="goToRegistration()">Зарегистрироваться</span>
-      </span>
-    </div>
-  </section>
+		<h1>Login</h1>
+		<div class="o-flex o-flex-column">
+			<OLabel caption="E-mail">
+				<OInput />
+			</OLabel>
+			<OLabel caption="Password">
+				<OInput />
+			</OLabel>
+			<OButton text="Войти" />
+			<span> Нет аккаунта? <span class="o-link" @click="goToRegistration()">Зарегистрироваться</span></span>
+		</div>
+	</section>
 </template>
 
 <script lang="ts" setup>
-import { OButton, OInput, OLabel } from 'orange-ui-kit';
-import { useRouter } from 'vue-router';
+import { OButton, OInput, OLabel } from "orange-ui-kit";
+import { useRouter } from "vue-router";
 
 const componentName = "LoginView";
 const className = "login-view";
@@ -26,8 +24,8 @@ const className = "login-view";
 const router = useRouter();
 
 const goToRegistration = () => {
-  router.push('/registration')
-}
+	router.push("/registration");
+};
 </script>
 
 <style lang="scss" module>
--- src/views/authorization/registration/RegistrationView.vue
+++ src/views/authorization/registration/RegistrationView.vue
@@ -1,41 +1,41 @@
 <template>
-  <section :class="$style[className]">
-    <h1>Регистрация</h1>
-    <div>
-      <OLabel caption="E-mail">
-        <OInput />
-      </OLabel>
-      <OLabel caption="Password">
-        <OInput />
-      </OLabel>
-      <OLabel caption="Repeat password">
-        <OInput />
-      </OLabel>
-      <OButton text="Зарегистрироваться"/>
-      <span>
-        Уже есть аккаунт? <span class="r-pointer" @click="goToLogin()">Войти</span>
-      </span>
-    </div>
-  </section>
+	<section :class="$style[className]">
+		<h1>Регистрация</h1>
+		<div class="o-flex o-flex-column" :class="$style[`${className}-wrapper`]">
+			<OLabel caption="E-mail">
+				<OInput />
+			</OLabel>
+			<OLabel caption="Password">
+				<OInput type="password" />
+			</OLabel>
+			<OLabel caption="Repeat password">
+				<OInput type="password" />
+			</OLabel>
+			<OButton class="o-p-8" text="Зарегистрироваться" />
+			<span> Уже есть аккаунт? <span class="o-link" @click="goToLogin()">Войти</span> </span>
+		</div>
+	</section>
 </template>
 
 <script lang="ts" setup>
-import { OButton, OInput, OLabel } from 'orange-ui-kit';
-import { useRouter } from 'vue-router';
+import { OButton, OInput, OLabel } from "orange-ui-kit";
+import { useRouter } from "vue-router";
 
-const componentName = 'RegistrationView';
-const className = 'registration-view';
+const componentName = "RegistrationView";
+const className = "registration-view";
 
 const router = useRouter();
 
 const goToLogin = () => {
-  router.push('/login')
-}
-
+	router.push("/login");
+};
 </script>
 
 <style lang="scss" module>
-$component: 'registration-view';
+$component: "registration-view";
 .#{$component} {
+	&-wrapper {
+		flex-direction: column;
+	}
 }
 </style>
